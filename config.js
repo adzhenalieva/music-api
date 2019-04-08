4943ee8ffc642fc59b9fcf1ea0c2c3f9ef69c37e
Aisalkyn Dzhenalieva <aidzhenalieva@gmail.com>
2019-04-08 11:53:22 +0600

    config.js created with rootpath

---
+++ config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const rootPath = __dirname;
+
+module.exports = {
+    rootPath,
+    uploadPathAlbum: path.join(rootPath, 'public/uploads/albums'),
+    uploadPathArtist: path.join(rootPath, 'public/uploads/artists'),
+    dbURL: 'mongodb://localhost/music',
+    mongoOptions: {useNewUrlParser: true}
+};
+
